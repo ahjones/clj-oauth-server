ed174bd7a5ff571a09c2d0c753401403f50a1bf1
Andrew Jones <me@andrewhjon.es>
2012-07-08 22:24:52 +0100

    Update to 1.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-oauth-server "1.0.1-SNAPSHOT"
+(defproject clj-oauth-server "1.0.2-SNAPSHOT"
   :description "OAuth server support for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
