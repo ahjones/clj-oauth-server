83e436dd41b37618e51cef46bc43d987a1808aab
Joe Littlejohn <joe.littlejohn@nokia.com>
2014-04-01 11:21:10 +0100

    Allow oauth params via the query string or form
    See http://tools.ietf.org/html/rfc5849#section-3.5.2

--- test/oauth_server/server_test.clj
+++ test/oauth_server/server_test.clj
@@ -21,25 +21,47 @@
   (is (= (os/parse-oauth-header "Basic realm=\"Secure Area\"") nil))
   (is (= (os/parse-oauth-header "") nil))
   (is (= (os/parse-oauth-header nil) nil))
-)
+  )
 
 (deftest
-  #^{:doc "Test extraction of oauth parameters."} 
+  #^{:doc "Test parsing of oauth values from request params."}
+  parse-oauth-requestparams
+  (is (= (os/parse-oauth-requestparams {:params {:oauth_consumer_key "0685bd9184jfhq22"
+                                                 :oauth_token "ad180jjd733klru7"
+                                                 :oauth_signature_method "HMAC-SHA1"
+                                                 :oauth_signature "wOJIO9A2W5mFwDgiDvZbTSMK/PY="
+                                                 :oauth_timestamp "137131200"
+                                                 :oauth_nonce "4572616e48616d6d65724c61686176"
+                                                 :oauth_version "1.0"
+                                                 :other-param "xxx"}})
+       {:oauth_consumer_key "0685bd9184jfhq22"
+        :oauth_token "ad180jjd733klru7"
+        :oauth_signature_method "HMAC-SHA1"
+        :oauth_signature "wOJIO9A2W5mFwDgiDvZbTSMK/PY="
+        :oauth_timestamp "137131200"
+        :oauth_nonce "4572616e48616d6d65724c61686176"
+        :oauth_version "1.0"})))
+
+
+(deftest
+  #^{:doc "Test extraction of oauth parameters."}
   oauth-params
   (is (= (os/oauth-params {:headers {}}) nil))
-  (is (= (os/oauth-params {:headers {:authorize "Basic realm=\"Secure Area\""}}) nil))
-  (is (= (os/oauth-params {:headers 
-    { :authorize "OAuth realm=\"http://sp.example.com/\", oauth_consumer_key=\"0685bd9184jfhq22\", oauth_token=\"ad180jjd733klru7\", oauth_signature_method=\"HMAC-SHA1\", oauth_signature=\"wOJIO9A2W5mFwDgiDvZbTSMK%2FPY%3D\", oauth_timestamp=\"137131200\", oauth_nonce=\"4572616e48616d6d65724c61686176\",oauth_version=\"1.0\""}}) 
-    { :oauth_consumer_key "0685bd9184jfhq22"
-      :oauth_token "ad180jjd733klru7"
-      :oauth_signature_method "HMAC-SHA1"
-      :oauth_signature "wOJIO9A2W5mFwDgiDvZbTSMK/PY="
-      :oauth_timestamp "137131200"
-      :oauth_nonce "4572616e48616d6d65724c61686176"
-      :oauth_version "1.0"
-    }))
-  )
-  
+  (is (= (os/oauth-params {:headers {"authorization" "Basic realm=\"Secure Area\""}}) nil))
+  (is (= (os/oauth-params {:headers
+    { "authorization" "OAuth realm=\"http://sp.example.com/\", oauth_consumer_key=\"0685bd9184jfhq22\", oauth_token=\"ad180jjd733klru7\", oauth_signature_method=\"HMAC-SHA1\", oauth_signature=\"wOJIO9A2W5mFwDgiDvZbTSMK%2FPY%3D\", oauth_timestamp=\"137131200\", oauth_nonce=\"4572616e48616d6d65724c61686176\",oauth_version=\"1.0\""}})
+    {:oauth_consumer_key "0685bd9184jfhq22"
+     :oauth_token "ad180jjd733klru7"
+     :oauth_signature_method "HMAC-SHA1"
+     :oauth_signature "wOJIO9A2W5mFwDgiDvZbTSMK/PY="
+     :oauth_timestamp "137131200"
+     :oauth_nonce "4572616e48616d6d65724c61686176"
+     :oauth_version "1.0"
+     }))
+  (is (= (os/oauth-params {:headers {}
+                           :params {:oauth_consumer_key "0685bd9184jfhq22"}})
+         {:oauth_consumer_key "0685bd9184jfhq22"})))
+
 (deftest
   request-method
   (is (= "GET"    (os/request-method {:request-method :get})))
